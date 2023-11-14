6a2bc6e2d2a77bce84cc44921c637e7c3d6bb4b8
Vedant Jain <jscafe.business@gmail.com>
2023-11-14 08:50:01 +0530

    Implement CRUD API

--- pages/api/task/index.ts
+++ pages/api/task/index.ts
@@ -3,17 +3,40 @@ import prisma from "@/lib/prisma";
 
 // Define the API route function
 async function handler(req: NextApiRequest, res: NextApiResponse) {
- if (req.method === "POST") {
+  if (req.method === "GET") {
     try {
-      const { title, description, status, email } = req.body;
-      console.log("create post", title, description, status, email);
-      const user = await prisma.user.findUnique({
-        where: { email },
-        select: { id: true },
-      });
+      const {email} = req.query
+      console.log("user email", email)
+      if (!email || (Array.isArray(email))) {
+        res.status(500).json({ error: "Invalid user" });
+        return 
+      }
+      const user = await prisma.user.findUnique({where: {email}, select: {id: true}})
       if (!user) {
         res.status(500).json({ error: "Invalid user" });
-        return;
+        return
+      }
+      const results = await prisma.task.findMany({
+        where: {userId: user.id},
+        orderBy: [
+          {
+            createdAt: "desc",
+          },
+        ],
+      });
+      return res.status(201).json(results);
+    } catch (e) {
+    
+      res.status(500).json({ error: "Error finding tasks" });
+    }
+  } else if (req.method === "POST") {
+    try {
+      const { title, description, status, email } = req.body;
+      console.log("create post", title, description, status, email)
+      const user = await prisma.user.findUnique({where: {email}, select: {id:true}})
+      if (!user) {
+        res.status(500).json({ error: "Invalid user" });
+        return
       }
       await prisma.task.create({
         data: {
@@ -24,7 +47,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
         },
       });
       const results = await prisma.task.findMany({
-        where: { userId: user.id },
+        where: {userId: user.id},
         orderBy: [
           {
             createdAt: "desc",
@@ -33,10 +56,35 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       });
       res.status(201).json({ tasks: results });
     } catch (e) {
-      console.log(e);
+        console.log(e)
       res.status(500).json({ error: "Error creating task" });
     }
-  }
+  } else if (req.method === "PATCH") {
+    try {
+      const { title, description, status, id } = req.body;
+      const updatedTask = await prisma.task.update({
+        data: {
+          title,
+          description,
+          status,
+        },
+        where: {
+          id,
+        },
+      });
+      res.status(200).json({ task: updatedTask });
+    } catch (e) {
+        res.status(500).json({ error: "Error updating task" });
+    }
+  } else if (req.method === "DELETE") {
+    try {
+        const { id } = req.body;
+        const deletedTask = await prisma.task.delete({where: {id}})
+        res.status(200).json({ task: deletedTask });
+    } catch(e) {
+        res.status(500).json({ error: "Error deleting task" });
+    }
+    }
 }
 
 export default handler;
